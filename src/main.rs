945678ac082649368d939152acd45753b5e1b455
Jan Kiene <jankiene@mailbox.org>
2026-04-04 15:11:08 +0200

    handle some errors directly inside main function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod wav2raw;
 
+use crate::wav2raw::Wav2RawError;
 use crate::wav2raw::copy_data;
 use crate::wav2raw::read_file_header;
+
 use clap::Parser;
 use std::fs::File;
 use std::path::PathBuf;
@@ -16,13 +18,45 @@ struct Args {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
-    println!("{} {}", args.infile.display(), args.outfile.display());
+    let mut infile = match File::open(&args.infile) {
+        Ok(file) => file,
+        Err(_) => {
+            println!("Can't open input file at {}.", args.infile.display());
+            return Ok(());
+        }
+    };
 
-    let mut infile = File::open(args.infile)?;
-    let mut outfile = File::create_new(args.outfile)?;
+    let mut outfile = match File::create_new(&args.outfile) {
+        Ok(file) => file,
+        Err(_) => {
+            println!("Can't open new output file at {}.", args.outfile.display());
+            return Ok(());
+        }
+    };
 
-    read_file_header(&mut infile)?;
+    match read_file_header(&mut infile) {
+        Err(Wav2RawError::CantReadHeader) => {
+            println!(
+                "Can't read full RIFF header (first 12 bytes) from {}.",
+                args.infile.display()
+            );
+            return Ok(());
+        }
+        Err(Wav2RawError::InvalidHeader) => {
+            println!(
+                "RIFF header of input file {} is invalid.",
+                args.infile.display()
+            );
+            return Ok(());
+        }
+        _ => {}
+    };
 
+    println!(
+        "Copying data from {} into {}...",
+        args.infile.display(),
+        args.outfile.display()
+    );
     copy_data(&mut infile, &mut outfile)?;
 
     Ok(())
